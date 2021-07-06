996ce34ce4df9fabd2ef7051548417749533073a
Mark-vas <vasyaev.mark@mail.ru>
2021-07-06 17:48:23 +0300

    Изменение 1

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className="header">
+            <img src="images/vk.png"></img>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function NavBar() {
+    return (
+        <nav className="nav">
+            <div>
+                <a>Profiles</a>
+                <a>Messages</a>
+                <a>News</a>
+                <a>Music</a>
+                <a>Settings</a>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
